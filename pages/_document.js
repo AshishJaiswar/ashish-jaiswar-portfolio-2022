98ba96daadb43964aaf35a7d80e89cf3747db798
AshishJaiswar <Ajaiswar123456789@gmail.com>
2022-08-22 17:11:12 +0530

    replaced http-eval... attribute

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="favicon-ashish-jaiswar.png" />
         {/* fonts  */}
