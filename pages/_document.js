c62a2315f2d4f00356eb13c0b2040b7c738b3271
AshishJaiswar <Ajaiswar123456789@gmail.com>
2022-08-23 20:14:05 +0530

    gtag error resolved

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="favicon-ashish-jaiswar.png" />
