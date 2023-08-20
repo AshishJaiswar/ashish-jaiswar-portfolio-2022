9f99664d2e0ad7fff7e5d0e419174a5ddc92194f
AshishJaiswar <Ajaiswar123456789@gmail.com>
2023-08-20 13:33:16 +0530

    Updated details on 2023

--- components/about.js
+++ components/about.js
@@ -16,23 +16,22 @@ export default function About() {
         <b>blogging</b> and learing new skills.
       </p>
       <p>
-        I&apos;m currently working as a automation enginner at <b>Infosys</b>{" "}
-        where i create automated script for health checks(web functionality
-        test) and processes using <b>python</b> and <b>selenium</b>. I have
-        implemented devops pipeline to schedule jobs on daily basis.
+        I&apos;m currently working as a Front-End developer at <b>TCS</b> where
+        i develop web applications using <b>React</b> and <b>Next.js</b>.
       </p>
-      <p>Here are a few technologies I&apos;ve been working:</p>
+      <p>Here are the list technologies I&apos;ve been working on:</p>
       <div className={style.recent__skills}>
         <p>
           • Python <br />
+          • Flask <br />
+          • Django <br />
           • Selenium <br />
-          • DevOps <br />
-          • Postman and API&apos;s <br />
-        </p>
-        <p>
           • JavaScript <br />
           • Next.js <br />
-          • Power BI <br />
+          • React <br />
+          • HTML <br />
+          • Tailwind CSS <br />
+          • MongoDB <br />
           • SQL Server <br />
         </p>
       </div>
@@ -57,6 +56,39 @@ export default function About() {
       <div className={style.work__experience}>
         <h3>Work experience</h3>
         <div className={style.company}>
+          <p>TCS</p>
+          <span className={style.space}></span>
+          <p>
+            Front-End Developer&nbsp;
+            <svg className={style.cal}>
+              <use href="sprite.svg#calendar-outline"></use>
+            </svg>
+            2023 - present
+          </p>
+        </div>
+        <div>
+          <p className={style.separator}>
+            <svg
+              width="30px"
+              height="30px"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              color="#606c76"
+            >
+              <path
+                d="M12 12.5a.5.5 0 100-1 .5.5 0 000 1zM12 20.5a.5.5 0 100-1 .5.5 0 000 1zM12 4.5a.5.5 0 100-1 .5.5 0 000 1z"
+                fill="#000000"
+                stroke="#000000"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+            </svg>
+          </p>
+        </div>
+        <div className={style.company}>
           <p>Infosys</p>
           <span className={style.space}></span>
           <p>
@@ -68,7 +100,7 @@ export default function About() {
           </p>
         </div>
       </div>
-      <Link href="showcase-resume.pdf">
+      <Link href="Ashish-Jaiswar-portfolio-Resume.pdf">
         <a className="button mt-2" target="_blank">
           Resume
         </a>
