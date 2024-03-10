c1eaf6b76d96b11ce91aab96e8f74873b40ebf89
AshishJaiswar <Ajaiswar123456789@gmail.com>
2024-03-10 19:37:42 +0530

    Use short.io url shortner for resume

--- components/Resume.js
+++ components/Resume.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Resume({ className }) {
   return (
     <Link
-      href="https://media.graphassets.com/22om2yeLQ1yv9Fx3ew83"
+      href="https://dw13.short.gy/vWs6oG"
       className={className}
       target="_blank"
     >
